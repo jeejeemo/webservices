a67c3cf22bdd865c400c737b8ed070015b2f568d
Jean-Mo <jmbitoun@gmail.com>
2017-09-21 11:31:47 +0200

    commit2

--- exercice2/script.js
+++ exercice2/script.js
@@ -1,12 +1,32 @@
-$.ajax({
-    url : "http://localhost/webservices/api",
-    method:"get",
-    dataType : "json", // specifie le type de data récupéré
-    success: function( data ){
-        console.log ( data);
-    },
-    error:function ( error){
-        console.log( data);
-    }
+$("button").click(function(){
+    var value = $(this).val();
+        
+        $.ajax({
+            url : "http://localhost/webservices/api",
+            method:"get",
+            dataType : "json",
+            data:{
+                festival_id : value
+            },
+            success: function( data ){
+               console.log ( data );
+            },
+            error:function ( error){
+                
+            }
+
+        })
+})
+
+// $.ajax({
+//     url : "http://localhost/webservices/api",
+//     method:"get",
+//     dataType : "json", // specifie le type de data récupéré
+//     success: function( data ){
+//         console.log ( data);
+//     },
+//     error:function ( error){
+        
+//     }
 
 })
--- index.php
+++ index.php
@@ -2,14 +2,21 @@
 require "flight/Flight.php";
 
 Flight::route ("/api", function(){
-
-        $festival = [
+        $festival_id = $_GET["festival_id"];
+        $festivals = [
+                "0"=> [
                 "name"=>"Electrobeach",
                 "lat"=>1.5213,
                 "long" => 2.563
+                ],
+                "1"=>[
+                "name"=>"Hellfest",
+                "lat"=>2.5213,
+                "long" => 3.563
+                ]
         ];
 
-        echo json_encode($festival); // transforme ne variable en Json
+        echo json_encode($festivals[$festival_id]); 
 });
 
 Flight::start();
